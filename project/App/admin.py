2a57acc783d96e244c7758254e3d15ca0c2661b1
esdandreu <esdandreu@gmail.com>
2019-11-21 17:37:46 +0100

    Working

--- project/App/admin.py
+++ project/App/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Task, Freelancer, ActiveMailbox, Campaign, CampaignCategory, TaskCategory
-
 # Register your models here.
-admin.site.register(Task)
-admin.site.register(Freelancer)
-admin.site.register(ActiveMailbox)
-admin.site.register(Campaign)
-admin.site.register(CampaignCategory)
-admin.site.register(TaskCategory)
+
